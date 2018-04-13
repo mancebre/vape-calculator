12a36fef5d12debf317a93464a1ffa8ac501882c
Djordje Mancovic <dj.mancovic@gmail.com>
2018-04-13 21:46:00 +0200

    Title change

--- public/app/watchers.js
+++ public/app/watchers.js
@@ -5,6 +5,6 @@ App.run(['$rootScope', '$route', function($rootScope, $route) {
      * On each route change, change page title.
      */
     $rootScope.$on('$routeChangeSuccess', function() {
-        document.title = $route.current.title + ' | Guided E-Learning';
+        document.title = $route.current.title + ' | Vapers Cuisine';
     });
 }]);
