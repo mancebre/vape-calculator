722826f5008110744880d11f458ec9c0c97615d8
Djordje Mancovic <dj.mancovic@gmail.com>
2018-04-13 23:38:21 +0200

    trying something with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,54 @@ module.exports = function(grunt) {
                     cssDir: 'public/stylesheets'
                 }
             }
-        }
+        },
+
+		//
+        copy: {
+            build: {
+                cwd: 'public',
+                src: [ '**' ],
+                dest: 'build',
+                expand: true
+            }
+        },
+        clean: {
+            build: {
+                src: [ 'build' ]
+            },
+            stylesheets: {
+                src: [ 'build/**/*.css', '!build/application.css' ]
+            },
+            scripts: {
+                src: [ 'build/**/*.js', '!build/application.js' ]
+            }
+        },
+        cssmin: {
+            build: {
+                files: {
+                    'build/application.css': [ 'build/**/*.css' ]
+                }
+            }
+        },
+        coffee: {
+            build: {
+                expand: true,
+                cwd: 'public',
+                src: [ '**/*.coffee' ],
+                dest: 'build',
+                ext: '.js'
+            }
+        },
+        uglify: {
+            build: {
+                options: {
+                    mangle: false
+                },
+                files: {
+                    'build/application.js': [ 'build/**/*.js' ]
+                }
+            }
+        },
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -43,6 +90,30 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.loadNpmTasks('grunt-contrib-compass');
+
+    // TODO NOT WORKING
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask(
+        'stylesheets',
+        'Compiles the stylesheets.',
+        [ 'cssmin' ]
+    );
+    grunt.registerTask(
+        'scripts',
+        'Compiles the JavaScript files.',
+        [ 'coffee', 'uglify' ]
+    );
+    grunt.registerTask(
+        'build',
+        'Compiles all of the assets and copies the files to the build directory.',
+        [ 'clean', 'copy', 'stylesheets', 'scripts' ]
+    );
+	// NOT WORKING END
 
 	grunt.registerTask('default', ['jshint', 'connect', 'watch', 'compass']);
 
