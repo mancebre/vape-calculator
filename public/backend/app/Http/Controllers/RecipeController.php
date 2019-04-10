dbde86ca781182864ce5ef30c9c059b866d14f1d
Djordje Mancovic <djordje.mancovic@hyperoptic.com>
2019-04-10 14:38:32 +0200

    Recipe update fix

--- public/backend/app/Http/Controllers/RecipeController.php
+++ public/backend/app/Http/Controllers/RecipeController.php
@@ -1,5 +1,7 @@
 <?php
 namespace App\Http\Controllers;
+
+use Log;
 use App\Rating;
 use App\Recipe;
 use App\RecipeFlavors;
@@ -121,48 +123,22 @@ class RecipeController extends Controller {
 
         }
 
-		if ($request->input('name')) {
-			$recipe->name = $request->input('name');
-		}
-		if ($request->input('username')) {
-			$recipe->username = $request->input('username');
-		}
-		if ($request->input('desired_strength')) {
-			$recipe->desired_strength = $request->input('desired_strength');
-		}
-		if ($request->input('pg')) {
-			$recipe->pg = $request->input('pg');
-		}
-		if ($request->input('vg')) {
-			$recipe->vg = $request->input('vg');
-		}
-		if ($request->input('nicotine_strength')) {
-			$recipe->nicotine_strength = $request->input('nicotine_strength');
-		}
-		if ($request->input('nicotine_pg')) {
-			$recipe->nicotine_pg = $request->input('nicotine_pg');
-		}
-		if ($request->input('amount')) {
-			$recipe->amount = $request->input('amount');
-		}
-		if ($request->input('nicotine_vg')) {
-			$recipe->nicotine_vg = $request->input('nicotine_vg');
-		}
-		if ($request->input('wvpga')) {
-			$recipe->wvpga = $request->input('wvpga');
-		}
-		if ($request->input('sleep_time')) {
-			$recipe->sleep_time = $request->input('sleep_time');
-		}
-        if ($request->input('comment')) {
-            $recipe->comment = $request->input('comment');
-        }
-//        if ($request->input('vapeReady')) {
-            $recipe->vape_ready = $request->input('vapeReady');
-//        }
-//        if ($request->input('private')) {
-            $recipe->private = $request->input('private');
-//        }
+        $nicotine = json_decode($request->nicotine);
+
+		$recipe->name = $request->name;
+		$recipe->desired_strength = $request->desired_strength;
+		$recipe->pg = $request->pg;
+		$recipe->vg = $request->vg;
+		$recipe->nicotine_strength = $nicotine->strength;
+		$recipe->nicotine_pg = $nicotine->pg;
+		$recipe->amount = $request->amount;
+		$recipe->nicotine_vg = $nicotine->vg;
+		$recipe->wvpga = $request->wvpga;
+		$recipe->sleep_time = $request->sleep_time;
+		$recipe->vape_ready = $request->vapeReady;
+        $recipe->comment = $request->comment;
+        $recipe->user_id = $user->user_id;
+        $recipe->private = $request->private;
 
         $inputFlavors = json_decode($request->input('flavor'));
 
