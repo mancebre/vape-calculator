d96aaa61c6b8f2f3c4a1ab6c4959a8aea19efb36
Djordje Mancovic <djordje.mancovic@hyperoptic.com>
2019-05-15 14:52:29 +0200

    fb_Ideja.png

--- public/app/modules/nic_shot/controller.js
+++ public/app/modules/nic_shot/controller.js
@@ -15,14 +15,29 @@ angular.module('gelApp.nicShot').controller('nicShotCtrl', ['$scope', function (
             && $scope.juice.concentrationOfNicShot !== null
             && $scope.juice.desiredStrength !== null) {
                 
-                // TODO Need more validation!!!
                 $scope.juice.resultMl = ($scope.juice.desiredStrength - $scope.juice.nicConcentration)
                 / ($scope.juice.concentrationOfNicShot - $scope.juice.desiredStrength) * $scope.juice.totalMl;
                 $scope.juice.resultMl = $scope.juice.resultMl.toFixed(2);
         }
+    };
+
+    $scope.validateDesiredStrenght = function() {
+        if($scope.juice.desiredStrength >= $scope.juice.concentrationOfNicShot
+            && $scope.juice.desiredStrength > 0) {
+            $scope.juice.desiredStrength = $scope.juice.concentrationOfNicShot - 1;
+        }
+    };
+
+    $scope.validateNicConcentration = function() {
+        if($scope.juice.desiredStrength < $scope.juice.nicConcentration
+            && $scope.juice.desiredStrength > 0) {
+            $scope.juice.desiredStrength = $scope.juice.nicConcentration;
+        }
     }
 
 	$scope.$watch('juice', function (newVal, oldVal) {
+        $scope.validateDesiredStrenght();
+        $scope.validateNicConcentration();
         $scope.calculate();
     }, true);
 }]);
